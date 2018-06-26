cfa681181e5ff20a351f78217599bac7f592e483
Andrew971 <sordier.andrew@gmail.com>
2018-06-25 22:21:26 -0700

    googl map integrated

--- client/src/Components/MobileUi/card/simpleCard.js
+++ client/src/Components/MobileUi/card/simpleCard.js
@@ -3,15 +3,16 @@ import styled from "styled-components";
 export default styled.div`
 position:absolute;;
 top:10vh;
+left:5%;
 width:90%;
 display:flex;
 height:8vh;
+align-self:center;
 align-items:center;
 justify-content:center;
 background:white;
 border: 2px solid black;
 box-shadow: 0px 1px 5px 0px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 3px 1px -2px rgba(0, 0, 0, 0.12);
 z-index:1;
-margin:auto;
 align-self: center;
 `;
--- client/src/Utils/HOCs/googleMap.js
+++ client/src/Utils/HOCs/googleMap.js
@@ -1,13 +1,114 @@
-import React from 'react';
+import React, {Component} from 'react';
+import SearchBox from "react-google-maps/lib/components/places/SearchBox";
+import {withScriptjs, withGoogleMap, GoogleMap, Marker} from "react-google-maps"
+import {SimpleCard} from '../../Components/MobileUi/card'
+import {TextField} from '../../Components/inputForm'
+class MyMapComponent extends Component {
+  constructor(props) {
+    super(props)
 
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+    this.state = {
+      zoom:12,
+      bounds: null,
+      lat: 41.9,
+      lng: -87.624,
+      center: {
+        lat: 41.9,
+        lng: -87.624
+      },
+      markers: []
+    }
+    this.searchBox = React.createRef()
+    this.map = React.createRef()
 
-const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+  }
+
+  componentDidMount = () => {
+    navigator
+      .geolocation
+      .getCurrentPosition((position) => {
+        const {longitude, latitude} = position.coords
+        this.setState({
+          center: {
+            lat: latitude,
+            lng: longitude
+          }
+        })
+      })
+  }
+
+  handleBoundsChange = () => {
+    this.setState({
+      bounds: this
+        .refs
+        .map
+        .getBounds(),
+      center: this
+        .refs
+        .map
+        .getCenter()
+    });
+  }
+
+  handlePlacesChanged=()=> {
+    const places = this.refs
+      .searchBox
+      .getPlaces();
+    const bounds = new window.google
+      .maps
+      .LatLngBounds();
+
+    places.forEach(place => {
+      if (place.geometry.viewport) {
+        bounds.union(place.geometry.viewport)
+      } else {
+        bounds.extend(place.geometry.location)
+      }
+    });
+    const nextMarkers = places.map(place => ({position: place.geometry.location}));
+
+    const nextCenter = nextMarkers.length > 0 ? nextMarkers[0].position : this.state.center;
+
+    this.setState({center: nextCenter, markers: nextMarkers, zoom: 18});
+    // refs.map.fitBounds(bounds);
+  }
+
+render() {
+const {isMarkerShown} = this.props
+const {center, bounds,zoom} = this.state
+
+return (
   <GoogleMap
-    defaultZoom={8}
-    defaultCenter={{ lat: -34.397, lng: 150.644 }}
-  >
-    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+    ref="map"
+    defaultZoom={12}
+    zoom={zoom}
+    center={center}
+    onBoundsChanged={this.handleBoundsChange}
+    defaultClickableIcons={false}
+    defaultOptions={{
+      streetViewControl: false,
+      scaleControl: false,
+      mapTypeControl: false,
+      panControl: false,
+      zoomControl: false,
+      fullscreenControl: false
+    }}
+    disableDefaultUI
+    >
+    <SearchBox
+      ref="searchBox"
+      bounds={bounds}
+      onPlacesChanged={this.handlePlacesChanged}
+      controlPosition={window.google.maps.ControlPosition.BOTTOM}
+      >
+      <SimpleCard>
+  <TextField type="text" label="hello"/>
+  </SimpleCard>
+    </SearchBox>
+    {isMarkerShown && <Marker position={center}/>}
   </GoogleMap>
-))
-  export default MyMapComponent
+)
+}
+}
+
+export default withScriptjs(withGoogleMap(MyMapComponent))
